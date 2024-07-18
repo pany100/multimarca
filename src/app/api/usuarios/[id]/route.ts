6fd3e17076c3de77753491d6fda728d89ce4296f
Luis Paniagua <pany100@gmail.com>
2024-07-18 20:10:24 -0300

    adding admins

--- src/app/api/usuarios/[id]/route.ts
+++ src/app/api/usuarios/[id]/route.ts
@@ -39,3 +39,102 @@ export async function GET(
     );
   }
 }
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const user = await prisma.usuario.findUnique({
+      where: { id: parseInt(params.id) },
+    });
+
+    if (!user) {
+      return NextResponse.json(
+        { error: "Usuario no encontrado" },
+        { status: 404 }
+      );
+    }
+    const { fullName, username, email, rolId, avatar } = await request.json();
+    const updatedUser = await prisma.usuario.update({
+      where: { id: user.id },
+      data: {
+        fullName: fullName,
+        username: username,
+        email: email,
+        rol: {
+          connect: { id: rolId },
+        },
+        avatar: avatar,
+      },
+      select: {
+        id: true,
+        fullName: true,
+        username: true,
+        email: true,
+        rol: true,
+        avatar: true,
+      },
+    });
+    return NextResponse.json(updatedUser);
+  } catch (error) {
+    console.error("Error al obtener información del usuario:", error);
+    return NextResponse.json(
+      { error: "Error al obtener información del usuario" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const user = await prisma.usuario.findUnique({
+      where: { id: parseInt(params.id) },
+    });
+
+    if (!user) {
+      return NextResponse.json(
+        { error: "Usuario no encontrado" },
+        { status: 404 }
+      );
+    }
+
+    // Obtener el ID del usuario autenticado del encabezado
+    const authUserId = request.headers.get("x-user-id");
+    if (user.id === parseInt(authUserId || "0")) {
+      return NextResponse.json(
+        { error: "No puedes borrarte a ti mismo" },
+        { status: 403 }
+      );
+    }
+    // Verificar si el usuario autenticado tiene el rol de Administrador
+    const authUser = await prisma.usuario.findUnique({
+      where: { id: parseInt(authUserId || "0") },
+      include: { rol: true },
+    });
+
+    if (!authUser || authUser.rol?.name !== "Administrador") {
+      return NextResponse.json(
+        { error: "Solo los administradores pueden eliminar usuarios" },
+        { status: 403 }
+      );
+    }
+
+    await prisma.usuario.delete({
+      where: { id: user.id },
+    });
+
+    return NextResponse.json({
+      msg: "Usuario eliminado exitosamente",
+    });
+  } catch (error) {
+    console.error("Error al eliminar usuario:", error);
+    return NextResponse.json(
+      { error: "No se pudo eliminar el usuario" },
+      { status: 500 }
+    );
+  }
+}
---
+++ src/app/api/usuarios/admins/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+import prisma from "src/lib/prisma";
+
+export async function GET(request: Request) {
+  try {
+    // Obtener todos los usuarios con rol "Administrador"
+    const adminUsers = await prisma.usuario.findMany({
+      where: { rol: { name: "Administrador" } },
+      select: {
+        id: true,
+        fullName: true,
+        username: true,
+        email: true,
+        avatar: true,
+        rol: true,
+      },
+    });
+
+    return NextResponse.json(adminUsers);
+  } catch (error) {
+    console.error("Error al obtener administradores:", error);
+    return NextResponse.json(
+      { error: "No se pudo obtener la lista de administradores" },
+      { status: 500 }
+    );
+  }
+}
