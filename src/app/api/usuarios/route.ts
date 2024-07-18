63be12e01ad86745d2e3213117252da2e3b68383
Luis Paniagua <pany100@gmail.com>
2024-07-18 18:00:48 -0300

    initial config

---
+++ src/app/api/usuarios/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import prisma from "src/lib/prisma";
+
+export async function GET() {
+  const usuarios = await prisma.usuario.findMany();
+  return NextResponse.json(usuarios);
+}
+
+export async function POST(request: Request) {
+  const { email, nombre } = await request.json();
+  const nuevoUsuario = await prisma.usuario.create({
+    data: {
+      email,
+      fullName,
+      username,
+      password,
+    },
+  });
+  return NextResponse.json(nuevoUsuario, { status: 201 });
+}
