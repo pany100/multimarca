351bc0842be238f360104ca8b5ad495c9cfa8424
Luis Paniagua <pany100@gmail.com>
2024-07-18 21:44:57 -0300

    updating users

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -82,8 +82,53 @@ async function main() {
       },
     });
   }
-
   console.log("Carga de roles completada.");
+
+  console.log("Iniciando carga de usuarios...");
+
+  const usuarios = [
+    {
+      fullName: "Admin",
+      username: "Admin",
+      email: "pany100@gmail.com",
+      rolId: 1,
+      avatar: "/images/avatars/1.png",
+      password: "Probando01",
+    },
+    {
+      fullName: "Lisandro Troglio",
+      username: "Liso",
+      email: "liso.troglio@gmail.com",
+      rolId: 1,
+      avatar: "/images/avatars/7.png",
+      password: "7yXLuicGpE7FxQ",
+    },
+    {
+      fullName: "Mariano Troglio",
+      username: "Nano",
+      email: "mot024@hotmail.com",
+      rolId: 1,
+      avatar: "/images/avatars/5.png",
+      password: "Fvx2NfEZCg6tFz",
+    },
+    {
+      fullName: "Gabriela Ateca",
+      username: "Gaby",
+      email: "gabriela.ateca@gmail.com",
+      rolId: 2,
+      avatar: "/images/avatars/2.png",
+      password: "8nD3xZoKmu32zK",
+    },
+  ];
+
+  for (const usuario of usuarios) {
+    await prismaClient.usuario.upsert({
+      where: { email: usuario.email },
+      update: usuario,
+      create: usuario,
+    });
+  }
+  console.log("Carga de usuarios completada.");
 }
 
 main()
