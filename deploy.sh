90a908e75fcde26379be753ef44533c90c6b6761
Luis Paniagua <pany100@gmail.com>
2025-02-10 00:37:26 -0300

    updating deploy

--- deploy.sh
+++ deploy.sh
@@ -20,20 +20,25 @@ echo "📦 Construyendo la aplicación localmente..."
 nvm use 18.17.0 || handle_error "Falló la carga de NVM"
 npm run build || handle_error "Falló el build local"
 
-echo "\n🔄 Conectando al servidor remoto..."
-echo "⏳ Ejecutando secuencia de comandos en el servidor..."
+if [ $? -ne 0 ]; then
+    handle_error "Falló la ejecución de comandos en el servidor"
+fi
+
+echo "\n📤 Copiando archivos de build al servidor..."
+echo "⏳ Esto puede tomar unos minutos dependiendo del tamaño..."
+scp -i "$PEM_PATH" -r ../multimarca/.next $REMOTE_HOST:$REMOTE_PATH/.next2 || handle_error "Falló la copia de archivos al servidor"
+
+echo "\n🔄 Deteniendo y actualizando la aplicación en el servidor..."
 ssh -i "$PEM_PATH" $REMOTE_HOST << 'EOF'
-  set -e  # Esto hará que el script se detenga si cualquier comando falla
+  set -e
   
-  # Cargar NVM y Node
+  # Cargar NVM y Node nuevamente
   export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
   
   cd multimarca || exit 1
-  
-  echo "📌 Cambiando a Node.js 18.17.0..."
   nvm use 18.17.0 || exit 1
-  
+
   echo "🛑 Deteniendo aplicación actual..."
   npm list -g pm2 || npm install -g pm2
   pm2 stop next-app || true
@@ -52,26 +57,10 @@ ssh -i "$PEM_PATH" $REMOTE_HOST << 'EOF'
   
   echo "🏗️  Ejecutando prebuild..."
   npm run prebuild || exit 1
-EOF
-
-if [ $? -ne 0 ]; then
-    handle_error "Falló la ejecución de comandos en el servidor"
-fi
-
-echo "\n📤 Copiando archivos de build al servidor..."
-echo "⏳ Esto puede tomar unos minutos dependiendo del tamaño..."
-scp -i "$PEM_PATH" -r ../multimarca/.next $REMOTE_HOST:$REMOTE_PATH || handle_error "Falló la copia de archivos al servidor"
-
-echo "\n🔄 Iniciando la aplicación en el servidor..."
-ssh -i "$PEM_PATH" $REMOTE_HOST << 'EOF'
-  set -e
   
-  # Cargar NVM y Node nuevamente
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-  
-  cd multimarca || exit 1
-  nvm use 18.17.0 || exit 1
+  echo "🔄 Actualizando carpeta .next..."
+  rm -rf .next
+  mv .next2 .next
   
   echo "🚀 Iniciando aplicación con PM2..."
   pm2 start npm --name "next-app" -- run start || exit 1
