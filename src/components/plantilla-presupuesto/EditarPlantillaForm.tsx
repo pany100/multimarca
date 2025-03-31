d133bce5be7f58ec34935bd73248d648f521c92e
Luis Paniagua <pany100@gmail.com>
2025-03-31 10:47:50 -0300

    edit plantilla finished

---
+++ src/components/plantilla-presupuesto/EditarPlantillaForm.tsx
@@ -0,0 +1,114 @@
+import usePlantillaForm from "@/hooks/plantilla/usePlantillaForm";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import schema from "./schema";
+
+import { SnackbarProvider } from "@/contexts/SnackbarContext";
+import { Box } from "@mui/material";
+import { FormProvider } from "react-hook-form";
+import CustomInputText from "../formV2/CustomInputText";
+import FormActions from "../orden-reparacion/formV2/commons/FormActions";
+import FormSection from "../orden-reparacion/formV2/commons/FormSection";
+import ReparacionTercerosSection from "../orden-reparacion/formV2/sections/reparacion-terceros/ReparacionTercerosSection";
+import RepuestosUsadosSection from "../orden-reparacion/formV2/sections/repuestos-usados/RepuestosUsadosSection";
+import TrabajosRealizadosSection from "../orden-reparacion/formV2/sections/trabajos-realizados/TrabajosRealizadosSection";
+
+type PlantillaPresupuesto = {
+  id: number;
+  nombre: string;
+  repuestosUsados: {
+    id: number;
+    ordenReparacionId: number;
+    stockId: number;
+    stock: {
+      id: number;
+      name: string;
+    };
+    precioCompra: number;
+    precioVenta: number;
+    unidadesConsumidas: number;
+  }[];
+  reparacionesDeTercero: {
+    id: number;
+    nombre: string;
+    precioCompra: number;
+    precioVenta: number;
+    proveedorId: number;
+    proveedor: {
+      id: number;
+      name: string;
+    };
+  }[];
+  trabajosRealizados: {
+    id: number;
+    ordenReparacionId: number;
+    descripcion: string;
+    precioUnitario: number;
+    diasParaRecordatorio?: number;
+  }[];
+  manoDeObra: number;
+};
+
+type Props = {
+  plantilla: PlantillaPresupuesto;
+};
+
+function EditarPlantillaForm({ plantilla }: Props) {
+  const methods = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      ...plantilla,
+      trabajosRealizados: plantilla.trabajosRealizados.map((trabajo) => ({
+        manoDeObra: { name: trabajo.descripcion },
+        precioUnitario: Number(trabajo.precioUnitario),
+        diasParaRecordatorio: trabajo.diasParaRecordatorio,
+      })),
+      repuestosUsados: plantilla.repuestosUsados.map((repuesto) => ({
+        stock: { id: repuesto.stockId, name: repuesto.stock.name },
+        precioCompra: Number(repuesto.precioCompra),
+        precioVenta: Number(repuesto.precioVenta),
+        unidadesConsumidas: repuesto.unidadesConsumidas,
+      })),
+      reparacionesDeTercero: plantilla.reparacionesDeTercero.map(
+        (reparacion) => ({
+          nombre: reparacion.nombre,
+          precioCompra: Number(reparacion.precioCompra),
+          precioVenta: Number(reparacion.precioVenta),
+          proveedor: {
+            id: reparacion.proveedorId,
+            name: reparacion.proveedor.name,
+          },
+        })
+      ),
+    },
+  });
+  const { handleSubmit } = methods;
+  const { onEditSubmit } = usePlantillaForm(plantilla.id);
+
+  return (
+    <SnackbarProvider>
+      <FormProvider {...methods}>
+        <form onSubmit={handleSubmit(onEditSubmit)}>
+          <Box sx={{ pb: 2 }}>
+            <CustomInputText name="nombre" label="Nombre de la plantilla" />
+          </Box>
+          <FormSection title="Repuestos Usados">
+            <RepuestosUsadosSection />
+          </FormSection>
+          <FormSection title="Reparación / Repuestos de terceros">
+            <ReparacionTercerosSection />
+          </FormSection>
+          <FormSection title="Trabajos Realizados">
+            <TrabajosRealizadosSection />
+          </FormSection>
+          <FormActions
+            href="/dashboard/plantilla-presupuesto"
+            label="Plantilla"
+          />
+        </form>
+      </FormProvider>
+    </SnackbarProvider>
+  );
+}
+
+export default EditarPlantillaForm;
--- src/hooks/plantilla/usePlantillaForm.ts
+++ src/hooks/plantilla/usePlantillaForm.ts
@@ -4,7 +4,7 @@ import { useFetch } from "@/contexts/FetchContext";
 import { useSnackbarContext } from "@/contexts/SnackbarContext";
 import { useRouter } from "next/navigation";
 
-function usePlantillaForm() {
+function usePlantillaForm(id?: number) {
   const { setSnackbar } = useSnackbarContext();
   const { authFetch } = useFetch();
   const router = useRouter();
@@ -45,7 +45,46 @@ function usePlantillaForm() {
     }
   };
 
-  return { onSubmit };
+  const onEditSubmit = async (data: any) => {
+    try {
+      const response = await authFetch(`/api/plantilla-presupuesto/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        setSnackbar({
+          open: true,
+          message: "Plantilla de presupuesto actualizada con éxito",
+          severity: "success",
+        });
+        router.push("/dashboard/plantilla-presupuesto");
+      } else {
+        const errorData = await response.json();
+        setSnackbar({
+          open: true,
+          message:
+            errorData.error ||
+            "Error al actualizar la plantilla de presupuesto",
+          severity: "error",
+        });
+      }
+    } catch (error) {
+      console.error("Error al enviar la solicitud:", error);
+      setSnackbar({
+        open: true,
+        message: `Error al realizar la solicitud de actualización: ${
+          error instanceof Error ? error.message : "Error desconocido"
+        }`,
+        severity: "error",
+      });
+    }
+  };
+
+  return { onSubmit, onEditSubmit };
 }
 
 export default usePlantillaForm;
